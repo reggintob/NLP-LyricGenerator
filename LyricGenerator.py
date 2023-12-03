69ad53af877f42b8e0fe686e4e18b7c9bf4dfe73
Anirudh1610 <107986074+Anirudh1610@users.noreply.github.com>
2023-12-03 22:49:41 +0530

    Update LyricGenerator.py

--- LyricGenerator.py
+++ LyricGenerator.py
@@ -1,6 +1,8 @@
-import numpy as np
-#stupid
-#hi
-#jhfsjek
-#this is Neha
-ndekd
+import requests
+import api_key
+import lyricsgenius
+
+client_access_token = api_key.client_access_token
+
+LyricsGenius = lyricsgenius.Genius(client_access_token)
+
