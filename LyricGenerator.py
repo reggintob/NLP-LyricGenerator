eeecd5bb8bc2be103c9ce4ff8b4e02cc07aa7083
Anirudh1610 <107986074+Anirudh1610@users.noreply.github.com>
2023-12-04 11:17:24 +0530

    Update LyricGenerator.py

--- LyricGenerator.py
+++ LyricGenerator.py
@@ -2,7 +2,26 @@ import requests
 import api_key
 import lyricsgenius
 
+def remove_brackets(text):
+
+    pattern = re.compile(r'\[.*?\]')
+
+    cleaned_text = re.sub(pattern, '', text)
+
+    return cleaned_text
+
 client_access_token = api_key.client_access_token
 
 LyricsGenius = lyricsgenius.Genius(client_access_token)
+
+artist = input()
+title = input()
+
+song = LyricsGenius.search_song(artist, title)
+
+lyrics = song.lyrics
+
+result_text = remove_brackets(lyrics)
+
+print(result_text)
 
