f0255bf8b1db836d1baeba1f8c122ae53db7d399
Anirudh1610 <107986074+Anirudh1610@users.noreply.github.com>
2023-12-13 22:28:30 +0530

    Update LyricGenerator.py
    word vectors added

--- LyricGenerator.py
+++ LyricGenerator.py
@@ -1,6 +1,19 @@
 import requests
 import api_key
 import lyricsgenius
+import re
+import spacy
+
+client_access_token = api_key.client_access_token
+LyricsGenius = lyricsgenius.Genius(client_access_token)
+
+nlp = spacy.load("en_core_web_md")
+
+artist = LyricsGenius.search_artist("Kanye West", max_songs=5)
+
+lyrics = ""
+for song in artist.songs:
+    lyrics += song.lyrics + "\n" 
 
 def remove_brackets(text):
 
@@ -10,18 +23,10 @@ def remove_brackets(text):
 
     return cleaned_text
 
-client_access_token = api_key.client_access_token
-
-LyricsGenius = lyricsgenius.Genius(client_access_token)
-
-artist = input()
-title = input()
-
-song = LyricsGenius.search_song(title, artist)
-
-lyrics = song.lyrics
-
 result_text = remove_brackets(lyrics)
 
-print(result_text)
+# print(result_text)
 
+doc = nlp(lyrics)
+
+word_vectors = [token.vector for token in doc if token.is_alpha]
