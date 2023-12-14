26e3d8d5d3575c13e529eb869c25d20a3ee6c335
Anirudh1610 <107986074+Anirudh1610@users.noreply.github.com>
2023-12-14 21:36:11 +0530

    Update LyricGenerator.py

--- LyricGenerator.py
+++ LyricGenerator.py
@@ -1,90 +1,101 @@
 import requests
-import api_key
 import lyricsgenius
 import re
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, Dropout
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 import spacy
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import api_key
+import random
+import nltk
+from nltk.corpus import cmudict
 
-
-
+# spacy.cli.download("en_core_web_md")
 client_access_token = api_key.client_access_token
+
 LyricsGenius = lyricsgenius.Genius(client_access_token)
 
 nlp = spacy.load("en_core_web_md")
 
-artist = "Taylor Swift"
-
-artist = LyricsGenius.search_artist(artist, max_songs=30)
+artist = LyricsGenius.search_artist("Taylor swift", max_songs=20)
 
 lyrics = ""
 for song in artist.songs:
     lyrics += song.lyrics + "\n" 
 
-def remove_brackets(text):
-    pattern = re.compile(r'\[.*?\]')
-    cleaned_text = re.sub(pattern, '', text)
-    return cleaned_text
+def remove_brackets(input_string):
 
-# Collect lyrics
-lyrics = ""
-for song in artist.songs:
-    lyrics += remove_brackets(song.lyrics) + "\n"
+    square_brackets_pattern = re.compile(r'\[.*?\]')
 
-# Tokenize and preprocess the lyrics data
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts([lyrics])
-total_words = len(tokenizer.word_index) + 1
-
-input_sequences = []
-token_list = tokenizer.texts_to_sequences([lyrics])[0]
-for i in range(1, len(token_list)):
-    n_gram_sequence = token_list[:i+1]
-    input_sequences.append(n_gram_sequence)
-
-max_sequence_length = max([len(x) for x in input_sequences])
-input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, padding='pre')
-
-X, y = input_sequences[:, :-1], input_sequences[:, -1]
-y = tf.keras.utils.to_categorical(y, num_classes=total_words)
-
-# Build a simple LSTM model
-model = Sequential()
-model.add(Embedding(total_words, 50, input_length=max_sequence_length-1))
-model.add(Bidirectional(LSTM(100, return_sequences=True)))
-model.add(Bidirectional(LSTM(100, return_sequences=True)))
-model.add(Bidirectional(LSTM(100)))
-model.add(Dense(total_words, activation='softmax'))
-
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-
-# Train for more epochs
-more_epochs = 10
-for epoch in range(more_epochs):
-    print(f"Epoch {epoch + 1}/{more_epochs}")
-    model.fit(X, y, epochs=1, verbose=1)
     
-    # Generate lyrics at each epoch
-    seed_text = "love"
-    next_words = 50
+    parentheses_pattern = re.compile(r'\([^)]*\)')
 
-    generated_lyrics = seed_text
-    for _ in range(next_words):
-        token_list = tokenizer.texts_to_sequences([seed_text])[0]
-        token_list = pad_sequences([token_list], maxlen=max_sequence_length-1, padding='pre')
-        predicted = np.argmax(model.predict(token_list, verbose=0))
-        output_word = ""
-        for word, index in tokenizer.word_index.items():
-            if index == predicted:
-                output_word = word
-                break
-        seed_text += " " + output_word
-        generated_lyrics += " " + output_word
+    output_string = re.sub(square_brackets_pattern, '', input_string)
 
-    print(generated_lyrics)
+    output_string = re.sub(parentheses_pattern, '', output_string)
+
+    output_string = ' '.join(output_string.split())
+
+    return output_string
+
+result_text = remove_brackets(lyrics)
+
+tokens = word_tokenize(result_text)
+
+artist = input()
+title = input()
+
+mahesh = LyricsGenius.search_song(title, artist)
+ramesh = mahesh.lyrics
+
+ganesh = remove_brackets(ramesh)
+suresh = word_tokenize(ganesh)
+length = len(suresh)
+
+
+pronouncing_dict = cmudict.dict()
+
+
+def count_syllables(word):
+    if word in pronouncing_dict:
+        return max([len(list(y for y in x if y[-1].isdigit())) for x in pronouncing_dict[word]])
+    else:
+        return 0
+
+def build_markov_model(corpus, order=2):
+    markov_model = {}
+    for i in range(len(corpus) - order):
+        context = tuple(corpus[i:i + order])
+        next_word = corpus[i + order]
+        if context in markov_model:
+            markov_model[context].append(next_word)
+        else:
+            markov_model[context] = [next_word]
+    return markov_model
+
+markov_model = build_markov_model(tokens, order=3)
+def generate_poetry(markov_model, length):
+    poetry = []
+    for i in suresh:
+        line = []
+        context = random.choice(list(markov_model.keys()))
+            
+        if context in markov_model:
+            for _ in range(1):  # Add a limit to the number of attempts to avoid infinite loop
+                next_word = random.choice(markov_model[context])
+
+                line_syllables = count_syllables(' '.join(line))
+                next_word_syllables = count_syllables(next_word)
+
+                if line_syllables + next_word_syllables <= length:
+                    line.append(next_word)
+                    context = tuple(list(context[1:]) + [next_word])
+                else:
+                    break
+            poetry.extend(line)
+
+    return ' '.join(poetry)
+    
+generated_poetry = generate_poetry(markov_model,length)
+print(generated_poetry)
